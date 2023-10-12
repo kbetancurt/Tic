aa3b9453c40ba25149888c0ddb9e66ef61f248df
kbetancurt <kbetancurt@correo.um.edu.uy>
2023-10-12 11:22:49 -0300

    Airport, Gates y Pistas

--- src/main/java/um/edu/uy/business/entities/Airport.java
+++ src/main/java/um/edu/uy/business/entities/Airport.java
@@ -2,21 +2,72 @@ package um.edu.uy.business.entities;
 
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.sql.Date;
+import java.util.List;
 
 
 @Entity
 @Table(name="airport")
 public class Airport {
     @Id
-    @GeneratedValue(generator="airport_ids")
-    @GenericGenerator(name="airport_ids", strategy = "increment")
-    public long id;
+    public long ICAO;
+
     public String name;
     public String IATA;
+    public String country;
 
+    @OneToMany(mappedBy = "airport", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    public List<Gates> gates;
+
+    @OneToMany(mappedBy = "airport", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    public List<Track> tracks;
+
+    public long getICAO() {
+        return ICAO;
+    }
+
+    public void setICAO(long ICAO) {
+        this.ICAO = ICAO;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIATA() {
+        return IATA;
+    }
+
+    public void setIATA(String IATA) {
+        this.IATA = IATA;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public List<Gates> getGates() {
+        return gates;
+    }
+
+    public void setGates(List<Gates> gates) {
+        this.gates = gates;
+    }
+
+    public List<Track> getTracks() {
+        return tracks;
+    }
+
+    public void setTracks(List<Track> tracks) {
+        this.tracks = tracks;
+    }
 }
